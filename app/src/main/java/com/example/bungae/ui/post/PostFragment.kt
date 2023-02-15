d05743f4abc72b02fc9713112fb305ddf0fce5c3
minig <miniguni66@naver.com>
2023-02-15 16:28:16 +0900

    HomeFragment로 이동 구현

--- app/src/main/java/com/example/bungae/ui/post/PostFragment.kt
+++ app/src/main/java/com/example/bungae/ui/post/PostFragment.kt
@@ -39,56 +39,12 @@ class PostFragment : Fragment() {
     private var auth: FirebaseAuth = FirebaseAuth.getInstance()
     private var db: FirebaseFirestore = FirebaseFirestore.getInstance()
     private var imageStorage: FirebaseStorage = Firebase.storage
-//    private var uriInfo: Uri? = null
 
     private var mainActivity: MainActivity? = null
 
     private val postViewModel by lazy {
         PostViewModel(auth, db, imageStorage)
-//        val postViewModel = ViewModelProvider(this).get(PostViewModel::class.java)
     }
-
-//    private val permissionList = arrayOf(
-//        Manifest.permission.CAMERA,
-//        Manifest.permission.READ_MEDIA_IMAGES
-//    )
-//
-//    private val requestMultiplePermission =
-//        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
-//            result.forEach {
-//                if (!it.value) {
-//                    Toast.makeText(context, "${it.key}권한 허용 필요", Toast.LENGTH_SHORT).show()
-//                }
-//            }
-//            openDialog()
-//        }
-
-//    private val readImage =
-//        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-//            it.data?.data?.let { uri ->
-//                requireActivity().contentResolver.takePersistableUriPermission(uri,
-//                    Intent.FLAG_GRANT_READ_URI_PERMISSION
-//                )
-//                Glide.with(this).load(uri).into(binding.imgLoad)
-//                uriInfo = uri
-//            }
-//        }
-//
-//    private val getTakePicture =
-//        registerForActivityResult(ActivityResultContracts.TakePicture()) {
-//            if (it) {
-//                uriInfo.let { binding.imgLoad.setImageURI(uriInfo) }
-//            }
-//        }
-
-//    private fun createImageFile(): Uri? {
-//        val now = SimpleDateFormat("yyMMdd_HHmm ss", Locale.KOREA).format(Date())
-//        val content = ContentValues().apply {
-//            put(MediaStore.Images.Media.DISPLAY_NAME, "img_$now.jpg")
-//            put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")
-//        }
-//        return requireActivity().contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, content)
-//    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -102,10 +58,6 @@ class PostFragment : Fragment() {
     ): View {
         _binding = FragmentPostBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-//        binding.btnAddImage.setOnClickListener {
-//            requestMultiplePermission.launch(permissionList)
-//        }
 
         binding.btnCompletion.setOnClickListener {
             postViewModel.insertFireStorage(
@@ -128,30 +80,6 @@ class PostFragment : Fragment() {
         return root
     }
 
-//    private fun openDialog() {
-//        val dialogLayout = layoutInflater.inflate(R.layout.dialog, null)
-//        val dialogBuild = AlertDialog.Builder(context).apply {
-//            setView(dialogLayout)
-//        }
-//        val dialog = dialogBuild.create().apply { show() }
-//
-//        val cameraAddBtn = dialogLayout.findViewById<Button>(R.id.btn_camera)
-//        val fileAddBtn = dialogLayout.findViewById<Button>(R.id.btn_file)
-//
-//        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
-//        intent.type = "image/*"
-//
-//        cameraAddBtn.setOnClickListener {
-//            uriInfo = createImageFile()
-//            getTakePicture.launch(uriInfo)
-//            dialog.dismiss()
-//        }
-//
-//        fileAddBtn.setOnClickListener {
-//            readImage.launch(intent)
-//            dialog.dismiss()
-//        }
-//    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
